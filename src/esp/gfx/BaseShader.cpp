7dfb15eddc823773b39122011e8c6933f103fc0f
Yili Zhao <yili@fb.com>
2019-07-18 23:33:36 -0700

    1st commit: -) Defined a base class for the shaders; -) Refactored GenericShader class; -) Created shader code in the "shaders" folder;

---
+++ src/esp/gfx/BaseShader.cpp
@@ -0,0 +1,38 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#include "BaseShader.h"
+
+// #include <Corrade/Containers/Reference.h>
+// #include <Magnum/GL/Context.h>
+#include <Magnum/GL/Shader.h>
+#include <Magnum/GL/Texture.h>
+#include <Magnum/GL/Version.h>
+
+namespace esp {
+namespace gfx {
+
+BaseShader::BaseShader(Magnum::GL::Version version, 
+                       const std::string& vertexShaderFilename, 
+                       const std::string& fragmentShaderFilename): glVersion_(version), vertexShaderFilename_(vertexShaderFilename), fragmentShaderFilename_(fragmentShaderFilename) {
+}
+
+void BaseShader::setGLVersion(Magnum::GL::Version version) {
+  glVersion_ = version;
+}
+
+void BaseShader::setVertexShaderFilename(std::string& filename) {
+  vertexShaderFilename_ = filename;
+}
+
+void BaseShader::setGeometryShaderFilename(std::string& filename) {
+  geometryShaderFilename_ = filename;
+}
+
+void BaseShader::setFragmentShaderFilename(std::string& filename) {
+  fragmentShaderFilename_ = filename;
+}
+
+} // namespace gfx
+} // namespace esp
---
+++ src/esp/gfx/BaseShader.h
@@ -0,0 +1,33 @@
+// Copyright (c) Facebook, Inc. and its affiliates.
+// This source code is licensed under the MIT license found in the
+// LICENSE file in the root directory of this source tree.
+
+#pragma once
+
+#include <string>
+#include <Magnum/GL/Version.h>
+#include <Magnum/GL/AbstractShaderProgram.h>
+
+namespace esp {
+namespace gfx {
+
+class BaseShader : public Magnum::GL::AbstractShaderProgram {
+ public:
+   BaseShader(Magnum::GL::Version version = Magnum::GL::Version::GL410,
+              const std::string& vertexShaderFilename = "",
+              const std::string& fragmentShaderFilename = "");
+   virtual ~BaseShader() {};
+   void setGLVersion(Magnum::GL::Version version);
+   void setVertexShaderFilename(std::string& filename);
+   void setGeometryShaderFilename(std::string& filename);
+   void setFragmentShaderFilename(std::string& filename);
+   virtual void loadShaders() = 0;
+ protected:
+   Magnum::GL::Version glVersion_ = Magnum::GL::Version::GL410;
+   std::string vertexShaderFilename_ = "";
+   std::string geometryShaderFilename_ = "";
+   std::string fragmentShaderFilename_ = "";
+};
+
+}  // namespace gfx
+}  // namespace esp
